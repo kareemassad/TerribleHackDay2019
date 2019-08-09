c16b18dfc1d1af7f192102eb6d22462fcba1010d
aloisclerc <alois.clerc@gmail.com>
2019-08-09 12:37:11 -0400

    functionality

--- checker.js
+++ checker.js
@@ -1,11 +1,19 @@
 var body = document.querySelector("body");
-var btn = document.querySelector("#btn");
-
+var btn = document.querySelector(".btn");
+var color;
 
 body.classList.add("bg");
 
 btn.addEventListener("click", function(){
     body.classList.remove("bg");
-    body.style.backgroundColor = "red";
+
+    color = "#FF0000";
+    body.style.backgroundColor = color;
+
+    setInterval(function(){
+        color = chroma.random();
+        body.style.backgroundColor = color;
+        
+    }, 50);
 
 });
