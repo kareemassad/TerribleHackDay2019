4f720ec5b98854aeae8efd716f9f6932e8377e57
aloisclerc <alois.clerc@gmail.com>
2019-08-09 13:38:55 -0400

    animation

--- checker.js
+++ checker.js
@@ -3,17 +3,46 @@ var btn = document.querySelector(".btn");
 var color;
 
 body.classList.add("bg");
+var bool = true;
 
-btn.addEventListener("click", function(){
+// while(bool){
+
+
+
+btn.addEventListener("click", function () {
     body.classList.remove("bg");
 
     color = "#FF0000";
     body.style.backgroundColor = color;
+    btn.textContent = "i am having a bad time";
 
-    setInterval(function(){
+
+    var timer = setInterval(function () {
         color = chroma.random();
         body.style.backgroundColor = color;
-        
+        anime({
+            targets: '.btn',
+            translateX: 1000,
+            translateY: 500,
+            duration: 300,
+            direction: 'alternate',
+            loop: true,
+            easing: 'easeInOutSine'
+        });
     }, 50);
 
+
+    setTimeout(function () {
+
+    }, 100);
+
+    btn.addEventListener("click", function () {
+        clearInterval(timer);
+        body.classList.add("bg");
+        btn.textContent = "again! again!";
+
+    });
+
+
 });
+// }
